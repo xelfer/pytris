03b32cd9b52741f5c693430712143d57a61e42a1
Nicolas Triantafillou <nick@triso.me>
2014-08-16 14:38:19 +1000

    left right movement complete

--- game.py
+++ game.py
@@ -2,36 +2,37 @@ import random
 
 class Game:
 	def __init__(self):
-		self.gameboard = [[0 for i in xrange(10)] for i in xrange(20)]
+		self.boardwidth = 10
+		self.boardheight = 20
+		self.gameboard = [['0' for i in xrange(self.boardwidth)] for i in xrange(self.boardheight)]
 		self.blocks = ['I', 'J', 'L', 'O', 'Z', 'T', 'S']
 		self.falling = False
 		self.screen_size = self.width, self.height = 500, 1000
 		self.screen_colour = 0,0,0
-		self.speed = 1000
+		self.speed = 500
 		self.timer = 0
-		self.winstatus = 0	
+		self.gameover = False
 
 	def getNewBlock(self):
 		self.falling = True
-		choice = random.choice(self.blocks)
-		if choice == 'I':
+		self.piece = random.choice(self.blocks)
+		if self.piece == 'I':
 			return self.IBlock()
-		elif choice == 'J':
+		elif self.piece == 'J':
 			return self.JBlock()
-		elif choice == 'L':
+		elif self.piece == 'L':
 			return self.LBlock()
-		elif choice == 'O':
+		elif self.piece == 'O':
 			return self.OBlock()
-		elif choice == 'S':
+		elif self.piece == 'S':
 			return self.SBlock()
-		elif choice == 'T':
+		elif self.piece == 'T':
 			return self.TBlock()	
-		elif choice == 'Z':
+		elif self.piece == 'Z':
 			return self.ZBlock()
-		
 
 	def IBlock(self):
-		block = [[0 for i in xrange(10)] for i in xrange(4)]
+		block = [['0' for i in xrange(10)] for i in xrange(4)]
 		block[0][4] = 'I'
 		block[1][4] = 'I'
 		block[2][4] = 'I'
@@ -39,7 +40,7 @@ class Game:
 		return block
 
 	def JBlock(self):
-		block = [[0 for i in xrange(10)] for i in xrange(3)]
+		block = [['0' for i in xrange(10)] for i in xrange(3)]
 		block[0][5] = 'J'
 		block[1][5] = 'J'
 		block[2][5] = 'J'
@@ -47,7 +48,7 @@ class Game:
 		return block
 
 	def LBlock(self):
-		block = [[0 for i in xrange(10)] for i in xrange(3)]
+		block = [['0' for i in xrange(10)] for i in xrange(3)]
 		block[0][4] = 'L'
 		block[1][4] = 'L'
 		block[2][4] = 'L'
@@ -55,7 +56,7 @@ class Game:
 		return block
 
 	def OBlock(self):
-		block = [[0 for i in xrange(10)] for i in xrange(2)]
+		block = [['0' for i in xrange(10)] for i in xrange(2)]
 		block[0][4] = 'O'
 		block[0][5] = 'O'
 		block[1][4] = 'O'
@@ -63,7 +64,7 @@ class Game:
 		return block
 
 	def ZBlock(self):
-		block = [[0 for i in xrange(10)] for i in xrange(2)]
+		block = [['0' for i in xrange(10)] for i in xrange(2)]
 		block[0][3] = 'Z'
 		block[0][4] = 'Z'
 		block[1][4] = 'Z'
@@ -71,7 +72,7 @@ class Game:
 		return block
 
 	def TBlock(self):
-		block = [[0 for i in xrange(10)] for i in xrange(2)]
+		block = [['0' for i in xrange(10)] for i in xrange(2)]
 		block[0][4] = 'T'
 		block[1][3] = 'T'
 		block[1][4] = 'T'
@@ -79,7 +80,7 @@ class Game:
 		return block
 
 	def SBlock(self):
-		block = [[0 for i in xrange(10)] for i in xrange(2)]
+		block = [['0' for i in xrange(10)] for i in xrange(2)]
 		block[0][5] = 'S'
 		block[0][4] = 'S'
 		block[1][4] = 'S'
--- tetris.py
+++ tetris.py
@@ -1,5 +1,6 @@
 import sys, pygame, pprint
 import game
+import re
 
 # game timer update, ticks in seconds
 def timer_update(g):
@@ -8,26 +9,100 @@ def timer_update(g):
 		g.timer = tmptime
 		gameLoop(g)
 
-
-		
 # the main game loop
 def gameLoop(g):
-	if g.falling == False:
-			g.winstatus = addNewBlock(g)
-			if (g.winstatus == 1):
-				print "Game Over"
+	if (g.gameover == False):
+		if g.falling == False:
+			addNewBlock(g)
+		moveFallingPieceDown(g)
+		drawGame(g)
+
+def moveFallingPieceDown(g):
+	g.gameboard.reverse()
+	stopgame = False
+	for y in range(len(g.gameboard)):
+		for x in range(len(g.gameboard[y])):
+			move = re.match(r'[A-Z]', g.gameboard[y][x], flags=0)
+			if move: # check if we can move the block
+				if (re.match(r'[a-z]', g.gameboard[y-1][x], flags=0)):
+					stopgame = True
+				elif y == 0:
+					stopgame = True
+
+	if (stopgame == False):
+		for y in range(len(g.gameboard)):
+			for x in range(len(g.gameboard[y])):
+				move = re.match(r'[A-Z]', g.gameboard[y][x], flags=0)
+				if move:
+					g.gameboard[y-1][x] = g.gameboard[y][x]
+					g.gameboard[y][x] = '0'
+	else:
+		stop(g)
+
+	g.gameboard.reverse()
 	drawGame(g)
 
 
+def moveFallingPieceLeft(g):
+	cantmove = False
+	for y in range(len(g.gameboard)):
+		for x in range(len(g.gameboard[y])):
+			move = re.match(r'[A-Z]', g.gameboard[y][x], flags=0)
+			if move: # check if we can move the block
+				if x == 0:
+					cantmove = True
+				elif (re.match(r'[a-z]', g.gameboard[y][x-1], flags=0)):
+					cantmove = True
+
+	if (cantmove == False):
+		for y in range(len(g.gameboard)):
+			for x in range(len(g.gameboard[y])):
+				move = re.match(r'[A-Z]', g.gameboard[y][x], flags=0)
+				if move:
+					g.gameboard[y][x-1] = g.gameboard[y][x]
+					g.gameboard[y][x] = '0'
+	drawGame(g)
+
+def moveFallingPieceRight(g):
+	cantmove = False
+	for y in range(len(g.gameboard)):
+		for x in range(len(g.gameboard[y])):
+			move = re.match(r'[A-Z]', g.gameboard[y][x], flags=0)
+			if move:
+				if x == 9:
+					cantmove = True
+				elif re.match(r'[a-z]', g.gameboard[y][x+1], flags=0):
+					cantmove = True
+	
+	if (cantmove == False):
+		for y in range(len(g.gameboard)):
+			xx=g.boardwidth-1
+			for x in range(0,g.boardwidth):
+				move = re.match(r'[A-Z]', g.gameboard[y][xx], flags=0)
+				if move:
+					g.gameboard[y][xx+1] = g.gameboard[y][xx]
+					g.gameboard[y][xx] = '0'
+				xx=xx-1	
+	drawGame(g)
+
+def stop(g):
+	for y in range(len(g.gameboard)):
+		for x in range(len(g.gameboard[y])):
+			if (re.match(r'[A-Z]', g.gameboard[y][x], flags=0)):
+				g.gameboard[y][x] = g.gameboard[y][x].lower()
+	g.falling = False
+
 # draws the game according to the array
 def drawGame(g):
-	iblock = pygame.image.load("I.png")
-	jblock = pygame.image.load("J.png")
-	lblock = pygame.image.load("L.png")
-	oblock = pygame.image.load("O.png")
-	sblock = pygame.image.load("S.png")
-	tblock = pygame.image.load("T.png")
-	zblock = pygame.image.load("Z.png")
+	g.screen.fill(g.screen_colour)
+
+	iblock = pygame.image.load("images/I.png")
+	jblock = pygame.image.load("images/J.png")
+	lblock = pygame.image.load("images/L.png")
+	oblock = pygame.image.load("images/O.png")
+	sblock = pygame.image.load("images/S.png")
+	tblock = pygame.image.load("images/T.png")
+	zblock = pygame.image.load("images/Z.png")
 	iblockrect = iblock.get_rect()
 	jblockrect = jblock.get_rect()
 	lblockrect = lblock.get_rect()
@@ -40,25 +115,25 @@ def drawGame(g):
 
 	for y in range(len(g.gameboard)):
 		for x in range(len(g.gameboard[y])):
-			if g.gameboard[y][x] == ('I' or 'i'):
+			if g.gameboard[y][x] == 'I' or g.gameboard[y][x] == 'i':
 				block = iblock
 				blockrect = iblockrect				
-			elif g.gameboard[y][x] == ('J' or 'j'):
+			elif g.gameboard[y][x] == 'J' or g.gameboard[y][x] == 'j':
 				block = jblock
 				blockrect = jblockrect
-			elif g.gameboard[y][x] == ('L' or 'l'):
+			elif g.gameboard[y][x] == 'L' or g.gameboard[y][x] == 'l':
 				block = lblock
 				blockrect = lblockrect
-			elif g.gameboard[y][x] == ('O' or 'o'):
+			elif g.gameboard[y][x] == 'O' or g.gameboard[y][x] == 'o':
 				block = oblock
 				blockrect = oblockrect
-			elif g.gameboard[y][x] == ('S' or 's'):
+			elif g.gameboard[y][x] == 'S' or g.gameboard[y][x] == 's':
 				block = sblock
 				blockrect = sblockrect
-			elif g.gameboard[y][x] == ('T' or 't'):
+			elif g.gameboard[y][x] == 'T' or g.gameboard[y][x] == 't':
 				block = tblock
 				blockrect = tblockrect
-			elif g.gameboard[y][x] == ('Z' or 'z'):
+			elif g.gameboard[y][x] == 'Z' or g.gameboard[y][x] == 'z':
 				block = zblock
 				blockrect = zblockrect
 
@@ -67,30 +142,33 @@ def drawGame(g):
 				blockrect.y = (y*blockrect.height)
 				g.screen.blit(block, blockrect)
 				block, blockrect = 0,0
-
+	
 	pygame.display.flip()
 
 
 # adds a new block to the array
 def addNewBlock(g):
 	blocktoadd = g.getNewBlock()
-	pprint.pprint(blocktoadd)
 
+	# check if the whole new block will fit before declaring game over
 	for y in range(len(blocktoadd)):
 		for x in range(len(blocktoadd[y])):
-			if blocktoadd[y][x] != 0:
-				g.gameboard[y][x] = blocktoadd[y][x]
-			elif blocktoadd[y][x] == 0:
-				pass
-			else: #block hit another, game over
-				return 1
-	return 0
+			if blocktoadd[y][x] != '0':
+				if g.gameboard[y][x] != '0':
+					g.gameover = True
+					pprint.pprint(g.gameboard)
 
-
+	if (g.gameover == False):
+		for y in range(len(blocktoadd)):
+			for x in range(len(blocktoadd[y])):
+				if blocktoadd[y][x] != '0':
+					if g.gameboard[y][x] == '0':
+						g.gameboard[y][x] = blocktoadd[y][x]
+				
 # main
 def main():
 	pygame.init()
-	pygame.display.set_caption("pytet")
+	pygame.display.set_caption("pytris")
 	g = game.Game()
 	# set up game
 	g.screen = pygame.display.set_mode(g.screen_size)
@@ -100,10 +178,19 @@ def main():
 	# main game loop
 	while 1:
 		for event in pygame.event.get():
-			if event.type == pygame.QUIT: sys.exit()
+			if event.type == pygame.QUIT: 
+				sys.exit()
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT:
+					moveFallingPieceLeft(g)
+				if event.key == pygame.K_RIGHT:
+					moveFallingPieceRight(g)
+				if event.key == pygame.K_DOWN:
+					moveFallingPieceDown(g)
+				if event.key == pygame.K_UP:
+					rotatePiece(g)
 
 		timer_update(g)
-
 
 if __name__ == "__main__":
     main()
